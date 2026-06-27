7a63d0a9a08f3357a2243052851d208cb4448b6b
Prayag Awasthi <awasthiprayag044@gmail.com>
2026-06-27 11:36:03 +0530

    CP-Algorithms case study

---
+++ sieve-of-eratosthenes.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define endl "\n
+
+// This code basically prints the sieve of eratosthenes in a certain manner
+
+int main() {
+    fastio;
+    int n; cin >> n;
+    vector<bool> is_prime(n+1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i <= n; i++) {
+        if (is_prime[i] && (long long)i * i <= n) {
+            for (int j = i * i; j <= n; j += i)
+                is_prime[j] = false;
+        }
+    }
+
+    for(int i = 2; i <= n; i++){
+        if(is_prime[i]) cout << i << " ";
+    }
+    cout << endl;
+    // Time complexity: O(n log log n)
+    // Space complexity: O(n)
+}
